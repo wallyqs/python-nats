2f6774fb41e850b8fb5a652a2f55fd8f130c7431
Waldemar Quevedo <wally@apcera.com>
2015-12-22 10:11:14 -0800

    Travis kick

--- script/test.sh
+++ script/test.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-#!/bin/sh
-set -e
-# check to see if gnatsd folder is empty
-if [ ! "$(ls -A $HOME/gnatsd)" ]; then
-    (
-	mkdir -p $HOME/gnatsd;
-	cd $HOME/gnatsd
-	wget https://github.com/nats-io/gnatsd/releases/download/v0.7.2/gnatsd-v0.7.2-linux-amd64.tar.gz -O gnatsd.tar.gz;
-	tar -xvf gnatsd.tar.gz;
-	export PATH=$HOME/gnatsd:$PATH
-    )
-else
-  echo 'Using cached directory.';
-fi
+export PYTHONPATH=$(pwd)
 
 pip install --upgrade pip
 pip install unittest2
 pip install -r requirements.txt
 
-export PYTHONPATH=$(pwd)
 python tests/client_test.py
 python tests/protocol_test.py
